edddeafc338eebd989016cf7377c9ad1e578314a
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-04-09 10:33:12 +0900

    add function

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+<body>
+
+
+<?php
+    // echo strlen("Hi Daniel");
+    // echo str_word_count("Hi Daniel");
+    // echo strrev("Hi Daniel");
+    // echo strpos("Hi Daniel","Daniel");
+    echo str_replace("Daniel","Jason","Hi Daniel");
+?>
+</body>
+</html>
